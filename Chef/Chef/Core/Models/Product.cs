e00d6a7d7cb15d8a71b56a0ad838f8f59debad12
yaroslav <netpywka14@gmail.com>
2021-11-25 11:58:21 +0300

    Added main classes

---
+++ Chef/Chef/Core/Models/Product.cs
@@ -0,0 +1,34 @@
+﻿using Chef.Core.Specifications;
+using System;
+
+namespace Chef.Core.Models
+{
+    public abstract class Product
+    {
+        private string _name;
+
+        public string Name
+        {
+            get => _name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException();
+                }
+
+                _name = value;
+            }
+        }
+
+        public Manufacturer Manufacturer { get; set; }
+
+        public Product(string name, Manufacturer manufacturer)
+        {
+            Name = name;
+            Manufacturer = manufacturer;
+        }
+
+        public abstract double CaloriePerHundred { get; }
+    }
+}
