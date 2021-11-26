c1b0c165fdec909b94ccccf59456df8119209b0b
yaroslav <netpywka14@gmail.com>
2021-11-26 11:47:23 +0300

    Added interface for Salad.

---
+++ Chef/Chef/Abstractions/ISalad.cs
@@ -0,0 +1,24 @@
+﻿using Chef.Core.Models;
+using Chef.Core.Models.Vegetables;
+using System;
+using System.Collections.Generic;
+
+namespace Chef.Abstractions
+{
+    public interface ISalad
+    {
+        string Name { get; set; }
+
+        double TotalCaloric { get; }
+
+        void Add(Ingredient ingredient);
+
+        void Remove(Ingredient ingredient);
+
+        IEnumerable<Vegetable> FindBy(Func<Vegetable, bool> func);
+
+        IEnumerable<Vegetable> SortBy<TKey>(Func<Vegetable, TKey> func);
+
+        IEnumerable<Vegetable> SortByDescending<TKey>(Func<Vegetable, TKey> func);
+    }
+}
