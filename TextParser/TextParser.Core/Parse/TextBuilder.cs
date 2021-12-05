9f7cc027db28db27956ddbe2c58d2ce7a1b20695
yaroslav <netpywka14@gmail.com>
2021-12-05 16:16:24 +0300

    Added classes for parsing text.

---
+++ TextParser/TextParser.Core/Parse/TextBuilder.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextParser.Abstractions;
+using TextParser.Core.Factory;
+using TextParser.Model;
+
+namespace TextParser.Core.Parse
+{
+    public class TextBuilder
+    {
+        private FactoryLetter factoryLetter;
+
+        private IText text;
+        private ISentence sentence;
+
+        public TextBuilder()
+        {
+            factoryLetter = new FactoryLetter();
+            text = new Text();
+            sentence = new Sentence();
+        }
+
+        public IText GetText => text;
+
+        public void Add()
+        {
+            text.Add(sentence);
+            sentence = new Sentence();
+        }
+
+        public void Add(IWord word)
+        {
+            if (word != null)
+            {
+                if (!string.IsNullOrEmpty(word.Value))
+                {
+                    sentence.Add(word);
+                }
+            }
+        }
+
+        public void Add(IPunctuation punctuation)
+        {
+            if (punctuation != null)
+            {
+                if (!string.IsNullOrEmpty(punctuation.Value))
+                {
+                    sentence.Add(punctuation);
+                }
+            }
+        }
+
+        public bool IsKeySign(IPunctuation punctuation)
+        {
+            return factoryLetter.IsEnd(punctuation) || factoryLetter.IsPunctuation(punctuation);
+        }
+
+        public void Action(IWord word, IPunctuation punctuation)
+        {
+            if (factoryLetter.IsPunctuation(punctuation))
+            {
+                Add(word);
+                Add(punctuation);
+            }
+            else if (factoryLetter.IsEnd(punctuation))
+            {
+                Add(word);
+                Add(punctuation);
+                Add();
+            }
+            else if (char.IsWhiteSpace(Convert.ToChar(punctuation.Value)))
+            {
+                Add(word);
+                Add(punctuation);
+            }
+        }
+    }
+}
---
+++ TextParser/TextParser.Core/Parse/TextReader.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using TextParser.Abstractions;
+using TextParser.Model;
+
+namespace TextParser.Core.Parse
+{
+    public class TextReader
+    {
+        private TextBuilder textBuilder;
+
+        private string path = ConfigurationManager.AppSettings.Get("inputFile");
+
+        public TextReader()
+        {
+            if (!File.Exists(path))
+            {
+                throw new ArgumentException();
+            }
+
+            textBuilder = new TextBuilder();
+        }
+
+        public IText Read()
+        {
+            using (var reader = new StreamReader(path, System.Text.Encoding.Default))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    IWord word = new Word();
+
+                    line = DeleteSpaces(line);
+
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        ISymbol symbol = new Symbol(line[i].ToString());
+
+                        if (!char.IsLetter(Convert.ToChar(symbol.Value)))
+                        {
+                            IPunctuation punctuation = new Punctuation();
+                            punctuation.Add(symbol);
+
+                            if (textBuilder.IsKeySign(punctuation))
+                            {
+                                punctuation.Value = FindWholeSign(punctuation, line, ref i);
+                            }
+
+                            textBuilder.Action(word, punctuation);
+
+                            word = new Word();
+                            continue;
+                        }
+
+                        word.Add(symbol);
+                    }
+                }
+            }
+
+            return textBuilder.GetText;
+        }
+
+        private string DeleteSpaces(string line)
+        {
+            var list = new List<string>();
+
+            foreach (var item in line.Split(' '))
+            {
+                if (!item.Equals(""))
+                {
+                    list.Add(item);
+                }
+            }
+
+            return string.Join(" ", list);
+        }
+
+        public string FindWholeSign(IPunctuation punctuation, string line, ref int i)
+        {
+            string result = punctuation.Value;
+            i++;
+            for (; i < line.Length; i++)
+            {
+                ISymbol symbol = new Symbol(line[i].ToString());
+                punctuation.Add(symbol);
+                if (textBuilder.IsKeySign(punctuation))
+                {
+                    result += line[i];
+                    continue;
+                }
+                else
+                {
+                    i--;
+                    punctuation.Remove(symbol);
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
