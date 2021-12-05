476ed25c86994e4cba1a2139f4000a1124f8f8de
yaroslav <netpywka14@gmail.com>
2021-12-05 16:23:31 +0300

    Added services for working with text and sentences.

---
+++ TextParser/TextParser.Core/Services/SentenceService.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TextParser.Abstractions;
+
+namespace TextParser.Core.Services
+{
+    public static class SentenceService
+    {
+        public static IEnumerable<IWord> QuestionSentenceByWordLengthDistinct(this IEnumerable<ISentence> tokens, int length)
+        {
+            var questioneSentences = tokens.Where(x => x.Tokens.Last().Value.Equals("?"));
+
+            var result = new HashSet<IWord>();
+
+            foreach (var item in questioneSentences)
+            {
+                var words = item.GetWords.Where(x => x.Length == length).ToList();
+
+                words.ForEach(x => result.Add(x));
+            }
+
+            return result.GroupBy(x => x.Value).Select(x => x.First()).ToList();
+        }
+    }
+}
---
+++ TextParser/TextParser.Core/Services/TextService.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TextParser.Abstractions;
+using TextParser.Core.Factory;
+
+namespace TextParser.Core.Services
+{
+    public static class TextService
+    {
+        public static IEnumerable<ISentence> SortSentencesByWordCount(this IText text)
+        {
+            return text.Sentences.OrderBy(x => x.CountWord);
+        }
+
+        public static void RemoveWordsStartWithConsonantByLength(this IText text, int length)
+        {
+            var factoryLetter = new FactoryLetter();
+
+            foreach (var item in text.Sentences)
+            {
+                var wordsForDelete = item.GetWords.Where(x => x.Length == length && factoryLetter.IsConsonant(x)).ToList();
+
+                wordsForDelete.ForEach(x => item.Remove(x));
+            }
+        }
+
+        public static void ReplaceWordByLength(this IText text, int index, int length, string substring)
+        {
+            var sentence = text[index];
+
+            var wordReplace = sentence.GetWords.Where(x => x.Length == length).ToList();
+
+            wordReplace.ForEach(x => x.Value = substring);
+        }
+    }
+}
