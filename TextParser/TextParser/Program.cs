6d1f91a2a8608c092e7f43e988654618246aed04
yaroslav <netpywka14@gmail.com>
2021-12-05 16:25:13 +0300

    Checking the operation of the application.

--- TextParser/TextParser/Program.cs
+++ TextParser/TextParser/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using TextParser.Abstractions;
+using TextParser.Core.Parse;
+using TextParser.Core.Services;
 
 namespace TextParser
 {
@@ -10,6 +9,42 @@ namespace TextParser
     {
         static void Main(string[] args)
         {
+            TextReader reader = new TextReader();
+
+            IText text = reader.Read();
+
+            FirstTaskTest(text);
+
+            Console.ReadLine();
+        }
+
+
+        private static void FirstTaskTest(IText text)
+        {
+            Console.WriteLine(text.ToString());
+
+            Console.WriteLine("Sentences in ascending order of word count.");
+            int i = 0;
+            foreach (var item in text.SortSentencesByWordCount())
+            {
+                Console.WriteLine($"{++i}) {item} - {item.CountWord}");
+            }
+
+            i = 0;
+            foreach (var item in text.Sentences.QuestionSentenceByWordLengthDistinct(5))
+            {
+                Console.WriteLine($"{++i}] {item.Value}");
+            }
+
+            Console.WriteLine("--------------------------------------");
+
+            text.RemoveWordsStartWithConsonantByLength(5);
+            Console.WriteLine(text.ToString());
+
+            Console.WriteLine("-----------------------------------------");
+
+            text.ReplaceWordByLength(2, 7, "Apple");
+            Console.WriteLine(text.ToString());
         }
     }
 }
