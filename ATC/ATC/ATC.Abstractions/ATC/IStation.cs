cca8112782aadce636164e7d6f5d782bd3964268
yaroslav <netpywka14@gmail.com>
2021-12-20 14:28:27 +0300

    Added intefaces and enum PortStatus.

---
+++ ATC/ATC/ATC.Abstractions/ATC/IStation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ATC.Abstractions.ATC
+{
+    public interface IStation
+    {
+        event Func<ITerminal, IClient> GetClient;
+
+        void Add(ITerminal terminal);
+
+        void Remove(ITerminal terminal);
+
+        void Call(ITerminal terminal);
+
+        void Answer(ITerminal terminal);
+    }
+}
