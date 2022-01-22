073ba014f8bbed4a53fcc3466acf9b264e04de62
yaroslav <netpywka14@gmail.com>
2022-01-23 00:32:29 +0300

    Refactoring entity models.

--- WebSales/WebSales.DAL/Models/Manager.cs
+++ WebSales/WebSales.DAL/Models/Manager.cs
@@ -4,7 +4,7 @@ namespace WebSales.DAL.Models
 {
     public class Manager
     {
-        public string Id { get; set; }
+        public int Id { get; set; }
 
         public string Name { get; set; }
 
